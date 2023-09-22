f958fc7fbf0d1088bf83d8cef1cc39b883da5cb1
MrDGuy <84729967+MrDGuy@users.noreply.github.com>
2023-09-22 09:58:47 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,1 @@
-let roll_1 = randint(1, 6)
-let roll_2 = randint(1, 6)
-adventure.addToTextlog("Roll 1: " + roll_1)
-adventure.addToTextlog("Roll 2: " + roll_2)
-if (roll_1 == roll_2) {
-    adventure.addToTextlog("You rolled doubles!")
-} else {
-    adventure.addToTextlog("You did not roll doubles")
-}
 
